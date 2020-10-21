bf556b97b3928890675140dc26f9f33552d28430
RamGeo <ginamarouli@gmail.com>
2020-10-21 22:49:18 +0300

    Positive_Negative_Zero.py

---
+++ Positive_Negative_Zero.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 17:03:54 2020
+
+@author: Gina
+"""
+
+
+#!/usr/bin/python
+num = float(input("Input a number: "))
+if num > 0:
+    print("it is possitive number")
+elif num == 0:
+    print("it is Zero")
+else:
+    print("it is negative number")
